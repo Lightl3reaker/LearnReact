e37e9c8dc6675c813be5dedf2ccdf45e25f8b2bc
unknown <lightbreaker812@gmail.com>
2024-01-23 13:56:02 +0630

    Updated

--- TicTackToe/src/App.js
+++ TicTackToe/src/App.js
@@ -1,3 +1,36 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import {useState} from 'react';
+
+const Square=({value,onSquareClick})=>{
+  
+  return (<button className="square" onClick={onSquareClick}>
+            {value}
+          </button>)
+}
+
+
+export default function Board() {
+  //state
+  const [squares,setSquare] =useState(Array(9).fill(null));
+  const handleClick=()=>{
+      const nextsquare=squares.slice();
+      nextsquare[0]='X';
+      setSquare(nextsquare);
+  }
+  return(<>
+          <div className="board-row">
+            <Square value={squares[0]} onSquareClick={handleClick}/>
+            <Square value={squares[1]} onSquareClick={handleClick}/>
+            <Square value={squares[2]} onSquareClick={handleClick}/>
+          </div>
+          <div className="board-row">              
+            <Square value={squares[3]} onSquareClick={handleClick}/>
+            <Square value={squares[4]} onSquareClick={handleClick}/>
+            <Square value={squares[5]} onSquareClick={handleClick}/>
+          </div>
+          <div className="board-row">
+            <Square value={squares[6]} onSquareClick={handleClick}/>
+            <Square value={squares[7]} onSquareClick={handleClick}/>
+            <Square value={squares[8]} onSquareClick={handleClick}/>
+          </div>
+        </>)
 }
