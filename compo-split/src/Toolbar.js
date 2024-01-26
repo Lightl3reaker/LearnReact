ba0c423269f9a4d128b4b0bfe509e1872d46ac31
unknown <lightbreaker812@gmail.com>
2024-01-26 22:58:32 +0630

    'Updated

---
+++ compo-split/src/Toolbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+class Toolbar extends React.Component{
+    render(){
+        return(
+            <div style={{background:'cyan',padding:10}}>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+export default Toolbar;
